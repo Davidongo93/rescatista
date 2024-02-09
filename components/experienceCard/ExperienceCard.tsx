7117bd2edcc2bdea2399a2609be915591bf5dc4a
David Orlando Miranda Roa <domirandar@unal.edu.co>
2024-02-09 11:26:22 -0500

    Cards responsive.

--- components/experienceCard/ExperienceCard.tsx
+++ components/experienceCard/ExperienceCard.tsx
@@ -6,14 +6,14 @@ const ExperienceCard = ({ imageSrc, alt, title, description, startDate, endDate 
   const dateRange = `${startDate} - ${endDate || "Presente"}`;
 
   return (
-    <div className="flex bg-black bg-opacity-50 text-white p-4 rounded-lg -full items-center mt-4 mb-4">
+    <div className="flex bg-black bg-opacity-50 text-white p-4 rounded-lg -full items-center mt-4 mb-4 p-2">
       {/* Condición para verificar si la vista es vertical (como en dispositivos móviles) */}
       {typeof window !== 'undefined' && window.innerWidth < window.innerHeight ? (
         <div className="flex flex-col w-full">
           <Image
             src={imageSrc}
             alt={alt}
-            className="rounded-full mx-auto"
+            className="rounded-lg"
             width={720}
             height={480}
             sizes="(max-width: 640px) 100vw,
@@ -21,14 +21,14 @@ const ExperienceCard = ({ imageSrc, alt, title, description, startDate, endDate 
               (max-width: 1536px) 33vw,
               25vw"
           />
-          <div className="flex flex-col mt-4">
+          <div className="flex flex-col mt-12 mb-4 bg-orange-800 bg-opacity-20 text-white p-4 rounded-lg">
             <h2 className="text-lg font-bold mb-1">{title}</h2>
             <p className="text-sm text-gray-300">{dateRange}</p>
             <p className="text-xl mt-1 max-w-lg">{description}</p>
           </div>
         </div>
       ) : (
-        <div className="flex flex-col w-full  mb-4">
+        <div className="flex flex-row w-full  mb-4">
           <Image
             src={imageSrc}
             alt={alt}
@@ -38,12 +38,12 @@ const ExperienceCard = ({ imageSrc, alt, title, description, startDate, endDate 
                   (max-width: 1280px) 50vw,
                   (max-width: 1536px) 33vw,
                   25vw"
-            className="rounded-full m-6"
+            className="rounded-lg"
           />
-          <div className="flex flex-col ml-4">
-            <h2 className="text-lg font-bold mb-1">{title}</h2>
-            <p className="text-sm text-gray-300">{dateRange}</p>
-            <p className="text-xl mt-1 max-w-lg">{description}</p>
+          <div className="flex flex-col ml-8 mt-12 mb-4 bg-orange-800 bg-opacity-20 text-white p-4 rounded-lg">
+            <h2 className="text-lg font-bold mb-2">{title}</h2>
+            <p className="text-sm text-gray-300 mb-2">{dateRange}</p>
+            <p className="text-lg mt-1 max-w-lg">{description}</p>
           </div>
         </div>
       )}
