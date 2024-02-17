cfca639f854d899424890a02b18bd4c0e4574d8f
David Orlando Miranda Roa <domirandar@unal.edu.co>
2024-02-16 20:52:17 -0500

    Fix: experience card

--- components/experienceCard/ExperienceCard.tsx
+++ components/experienceCard/ExperienceCard.tsx
@@ -10,18 +10,17 @@ const ExperienceCard = ({ imageSrc, alt, title, description, startDate, endDate 
       {/* Condición para verificar si la vista es vertical (como en dispositivos móviles) */}
       {typeof window !== 'undefined' && window.innerWidth < window.innerHeight ? (
         <div className="flex flex-col w-full">
-          <Image
-            src={imageSrc}
-            alt={alt}
-            className="rounded-lg"
-            width={720}
-            height={480}
-            sizes="(max-width: 640px) 100vw,
-              (max-width: 1280px) 50vw,
-              (max-width: 1536px) 33vw,
-              25vw"
-          />
-          <div className="flex flex-col mt-12 mb-4 bg-orange-800 bg-opacity-20 text-white p-4 rounded-lg">
+<Image
+  src={imageSrc}
+  alt={alt}
+  className="rounded-lg"
+  width={480}
+  height={720}
+  layout="responsive"
+  objectFit="cover"
+/>
+
+          <div className="flex flex-col  bg-orange-800 bg-opacity-20 text-white p-4 rounded-lg">
             <h2 className="text-lg font-bold mb-1">{title}</h2>
             <p className="text-sm text-gray-300">{dateRange}</p>
             <p className="text-xl mt-1 max-w-lg">{description}</p>
@@ -29,18 +28,16 @@ const ExperienceCard = ({ imageSrc, alt, title, description, startDate, endDate 
         </div>
       ) : (
         <div className="flex flex-row w-full  mb-4">
-          <Image
-            src={imageSrc}
-            alt={alt}
-                width={720}
-                height={480}
-                sizes="(max-width: 640px) 100vw,
-                  (max-width: 1280px) 50vw,
-                  (max-width: 1536px) 33vw,
-                  25vw"
-            className="rounded-lg"
-          />
-          <div className="flex flex-col ml-8 mt-12 mb-4 bg-orange-800 bg-opacity-20 text-white p-4 rounded-lg">
+<Image
+  src={imageSrc}
+  alt={alt}
+  className="rounded-lg"
+  width={480}
+  height={720}
+  style={{ objectFit: 'cover', borderRadius: '0.5rem' }}
+/>
+
+          <div className="flex flex-col ml-8 bg-orange-800 bg-opacity-20 text-white p-4 rounded-lg">
             <h2 className="text-lg font-bold mb-2">{title}</h2>
             <p className="text-sm text-gray-300 mb-2">{dateRange}</p>
             <p className="text-lg mt-1 max-w-lg">{description}</p>
